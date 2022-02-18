23b189e1be56a9adb9af9cbd2f1f06498e1a78fe
jrudar <37217429+jrudar@users.noreply.github.com>
2022-02-18 02:57:52 -0500

    Add files via upload

--- LANDMark/__init__.py
+++ LANDMark/__init__.py
@@ -1,1 +1,7 @@
+"""
+This module imports LANDMark and associated classifiers
+"""
 
+from .LANDMark import LANDMarkClassifier, StatisticalLANDMarkClassifier
+
+__all__ = ['LANDMarkClassifier', "StatisticalLANDMarkClassifier"]
